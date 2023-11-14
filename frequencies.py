ad89a2e5362784e92a1d8e6968dd229d69b4337d
root <root@DESKTOP-TKAH30G>
2023-11-14 05:42:35 +0000

    finish the coding in frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,15 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for x in items:
+        x=str(x)
+        if x in frequencies:
+            frequencies[x] += 1
+        else:
+            frequencies[x] = 1
+
     return frequencies
